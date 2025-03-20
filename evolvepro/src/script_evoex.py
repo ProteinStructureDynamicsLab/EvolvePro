6a36d4bfebb315239708f3a1c8b2b04f4fcc7652
spiral-out22 <valente90@hotmail.it>
2025-03-20 16:50:48 +0100

    updated script coherently with funct used

---
+++ evolvepro/src/script_evoex.py
@@ -0,0 +1,47 @@
+from evolvepro.src.evolve import evolve_experimental, evolve_experimental_multi
+from evolvepro.src.data import create_csv
+
+iteration = 'number of iterations(int)'
+protein_name = 'protein_name'
+embeddings_base_path = 'path/to/embd'
+embeddings_file_name = 'embd_file_name'
+round_base_path = 'path_to_rounds'
+wt_fasta_path = 'path/to/wt/fasta'
+number_of_variants = 'variants_per_round(int)'
+output_dir = 'path/to/output/'
+reference_file = 'path/to/file'
+
+for i in range(iteration):
+
+	if i == 0 :
+	round_name = 'Round1'
+	round_file_names = [f"{protein_name}_Round1.csv"]
+	
+	rename_WT = False
+	
+	else:
+
+	# Single variant
+	round_name = f'Round{i+1}'
+	round_file_names = []
+
+	rename_WT = False
+
+	evolve_experimental(
+	    protein_name,
+	    round_name,
+	    embeddings_base_path,
+	   embeddings_file_name,
+	    round_base_path,
+	    round_file_names,
+	    wt_fasta_path,
+	    rename_WT,
+	    number_of_variants,
+	    output_dir
+	)
+
+	path_to_output = os.path.join((output_dir, protein_name, round_name , 'df_test.csv'))
+
+	next_round_name = f'{protein_name}_Round{i+2}'
+
+	create_csv(path_to_output , reference_file , number_of_variants , round_file_names , next_round_name)
